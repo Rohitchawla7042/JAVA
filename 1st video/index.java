dacdb33fef5105f52386988636675840ef90d276
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-09-20 22:21:46 +0530

    Update and rename index.js to index.java

---
+++ 1st video/index.java
@@ -0,0 +1,26 @@
+class Main {
+	public static void main(String args[]) {
+		// Your code goes here
+		System.out.println("Hello World");
+		System.out.println(10+20);
+		System.out.println("\"Hey , are you there\"");
+		System.out.println(10);
+		System.out.print(20);
+		System.out.print( 40);
+		System.out.println(80);
+		
+		System.out.println("Hello Guys : )");
+		System.out.print("Good Morning:");
+	}
+}
+// comments 
+
+//  single line comment 
+
+/*
+multi line comment 
+
+*/ 
+// java is case sensitive 
+
+
