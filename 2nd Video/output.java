99508108d3a42f9d08c75e4b2c6263320a4bb3ec
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-09-25 16:59:47 +0530

    Update and rename index.java to output.java

---
+++ 2nd Video/output.java
@@ -0,0 +1,42 @@
+System.out.println("Hello 40");
+		System.out.print("");
+        System.out.print("50 World !");
+		
+		int a =10;
+
+int b = 20;
+
+int c = 30;
+
+System.out.print(a);
+
+System.out.println(b);
+
+System.out.print(c);
+int g = 9;
+System.out.print(g * 3);
+
+System.out.println("a" +"b");
+
+// System.out.println("a" + 2);
+
+// System.out.println("2" + 2) * 3);
+
+// variables 
+
+int x = 2 ; 
+int y = 3; 
+
+int z = x+y; 
+
+System.out.println("Sum of " + x + " and " + y + " is "  + z);
+        System.out.println(1 + " " + 3 + " " + 5 + " " + 7 + " " + 9);
+int r = 5 + 5 * 2 + 2 * 2 + (2 * 3);	
+System.out.println(r);	
+
+int m = 10 ; 
+System.out.print(m+1);
+System.out.print(m);
+		
+	}
+}
