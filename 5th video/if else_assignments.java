bd692427406f08a5cfa3aaeb1312a26e52142982
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-01 13:23:55 +0530

    Update if else_assignments.java

--- 5th video/if else_assignments.java
+++ 5th video/if else_assignments.java
@@ -1,1 +1,176 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+	public static void main(String args[]) {
+		
 
+
+Scanner scn = new Scanner(System.in);
+// IF Else 1 st example 
+/*
+int age = scn.nextInt();
+if(age >= 18){
+	System.out.println("do vote"); 
+}
+else{
+	System.out.println("no vote");
+}
+
+
+// Prints the user is veg or non-veg 
+
+boolean non_veg = scn.nextBoolean();
+
+// user will type true if the user is non_veg
+
+if(non_veg == true){
+    System.out.println("Checken , Fish , etc");
+}
+else{
+    System.out.println("Pules , curd"); 
+}
+
+
+// CHECK TEMP AND CHECK IS THE USER IS HAVING FEVER OR NOT 
+
+// So when we are giving input of 98.6 our output is coming as no fever but it should come as fever as we temp is float and the input which we give that is double so we have to append fin temp
+
+float temp = scn.nextFloat(); 
+if (temp >= 98.6f){
+    System.out.println("fever");
+}
+else{
+    System.out.println("No Fever"); 
+}
+
+
+
+// else is only applicable for last if 
+
+// check whether the given nu is odd or not 
+int n1 = scn.nextInt(); 
+if (n1 % 2 == 0){
+    System.out.println(n1 + " is an even number");
+}
+else{
+        System.out.println(n1 + " is an odd number");
+
+}
+ 
+int a = 10 ; 
+boolean b = a++ > 10; 
+System.out.println(a);
+System.out.println(b);
+
+ 
+//  Assignments 
+
+// Q1
+
+int n1 = scn.nextInt(); 
+int n2 = scn.nextInt(); 
+if(n1 > n2){
+    System.out.println(n1);
+}
+else{
+        System.out.println(n2);
+
+}
+
+// Q2 
+
+  int n1 = scn.nextInt(); 
+        int n2 = scn.nextInt(); 
+        if(n1 < n2){
+            System.out.println(n1);
+}
+        else{
+            System.out.println(n2);
+
+}
+ Q3
+ 
+ int number = scn.nextInt(); 
+ if (number % 2 == 0){
+        System.out.println(0);
+
+ }
+ else{
+     System.out.println(1);
+ }
+
+
+// Q4 
+
+int cost_price = scn.nextInt(); 
+int sell_price = scn.nextInt(); 
+int count = 0 ; 
+int total_profit = 0 ; 
+int total_profit1 = 0 ; 
+if (cost_price < sell_price){
+    count++ ; 
+    System.out.println(count); 
+}
+if (count == 1){
+    total_profit = sell_price - cost_price ;
+    System.out.println(total_profit); 
+}
+if (count == 0){
+    count--;
+    System.out.println(count);
+    total_profit1 = cost_price - sell_price ; 
+    System.out.println(total_profit1);
+}
+
+Q5 
+
+int n1 = scn.nextInt(); 
+if (n1 % 5 == 0 && n1 % 11 == 0){
+    System.out.println(1);
+}
+else{
+    System.out.println(0);
+}
+
+
+
+// Q6 
+
+int age = scn.nextInt();
+if(age >= 13){
+	System.out.println("You can ride the roller coaster!"); 
+}
+else{
+	System.out.println("You can't ride the roller coaster.");
+}
+
+int k=20;
+
+if(k)
+
+{
+
+System.out.println("YES");
+
+}
+
+else
+
+{
+
+System.out.println("NO");
+
+}
+*/
+if(1)
+
+{
+
+System.out.println("OK");
+
+}
+
+	}
+
+
+}
