3ad460d6aa4e9aa2d7c0a05765d93ec8202d24f8
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-03 20:09:06 +0530

    Update else if.java

--- If -else2_6th video/else if.java
+++ If -else2_6th video/else if.java
@@ -1,1 +1,114 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+        // IF-Else2 
+        /*
+        Q1
+        int n1 = scn.nextInt(); 
+        if(n1 > 0){
+            System.out.println("Positive"); 
+        }
+        if(n1 < 0){
+            System.out.println("Negative"); 
+        }
+        if(n1 ==0){
+            System.out.println("Zero"); 
+        }
+        
+        Q2
+        int salary = scn.nextInt(); 
+        int rating = scn.nextInt(); 
+        int new_salary ; 
+        if(rating == 1){
+            System.out.println("No meet");
+        }
+        else if(rating == 2){
+            System.out.println("Met expectations");
+        }
+        else if(rating == 3){
+            new_salary = salary + (salary * 5/100) ; 
+            System.out.println("your rating is " + rating +  " and your new salary is " + new_salary);
+            
+        }
+        else if(rating == 4){
+            new_salary = salary + (salary * 10/100) ; 
+            System.out.println("your rating is " + rating +  " and your new salary is " + new_salary);
+            
+        }
+        else if(rating == 5){
+           new_salary = salary + (salary * 15/100) ; 
+            System.out.println("your rating is " + rating +  " and your new salary is " + new_salary);
+            
+        }
+        else{
+            System.out.println("No right rating!!!");
+        }
+        
+      Q3
+        int side_a = scn.nextInt(); 
+        int side_b = scn.nextInt(); 
+        int side_c = scn.nextInt(); 
+        int total_area = side_a + side_b + side_c ; 
+        if (total_area == 180){
+            
+            System.out.println("Vaild triangle"); 
+        }
+        System.out.println(total_area);
 
+int N = 5;
+if(N>2)
+    System.out.println("Yayay");
+else
+    System.out.println("Blahblah!!");
+      
+      Q4 
+      int side_a = scn.nextInt(); 
+      int side_b = scn.nextInt(); 
+      int side_c = scn.nextInt(); 
+      
+      if(side_a == side_b && side_b == side_c){
+          System.out.println("Equilateral "); 
+      }
+      else if(side_a == side_b || side_b == side_c || side_a == side_c){
+          System.out.println("Isocloses");
+      }
+      else{
+          System.out.println("Scalene");
+      }
+      
+    //   for getting boolean we need relational operators not assignment . 
+  
+       
+    // Q5 
+    
+    int a = scn.nextInt(); 
+    if (a % 3 == 0 && a % 5 == 0){
+        System.out.println("Fizzbuzz"); 
+    }
+    else if(a%3==0){
+        System.out.println("Fizz"); 
+    }
+    else if(a % 5==0){
+        System.out.println("Buzz");
+    }
+    
+    Q6 
+    int n = scn.nextInt();
+            if(n%3 == 0){
+   System.out.println("Divisible by 3");
+}
+else if(n%5 == 0){
+   System.out.println("Divisible by 5");
+}
+else{
+   System.out.println("Divisible by both");
+}
+
+     */
+    
+	}
+
+
+}
