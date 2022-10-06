1581fdff861a5c3a842e7892158fb49e33672703
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-06 23:59:11 +0530

    Update Homework..java

--- if-else3/Homework..java
+++ if-else3/Homework..java
@@ -1,1 +1,41 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+	public static void main(String args[]) {
+     You are given a Bank account having N amount and you are asked to perform ADD(credit) or SUBTRACT(debit) operation of an amount X.
 
+After the operation print the amount left in the Bank account. If the debit amount is greater than current balance print "Insufficient Funds"(without quotes) and the operation is skipped.
+
+    int initial_balance = scn.nextInt(); 
+    int type = scn.nextInt() ; 
+    int amount = scn.nextInt(); 
+    // int total_balance ; 
+    
+    if (type == 1){
+        initial_balance = initial_balance + amount ; 
+        System.out.println(initial_balance);
+    }
+    else if(type == 2){
+        initial_balance = initial_balance - amount ; 
+        if(initial_balance < 0){
+            System.out.println("Insufficient Funds");
+        }
+        else {
+            System.out.println(initial_balance); 
+        }
+    }
+    
+    */
+    
+      Q2 
+      
+      
+
+
+        
+        
+        
+        
+    }
+        
+	}
