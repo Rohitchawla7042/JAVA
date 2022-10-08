0159a5a9fe10b98f3203e9dbd5126d5ee8c2991b
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-08 21:49:17 +0530

    Update assignment.java

--- While loop -1/assignment.java
+++ While loop -1/assignment.java
@@ -1,1 +1,76 @@
+//  Assignments
+
+// Q 1
+
+// Write a program to print all Natural numbers from 1 to N where you have to take N as input from user
+
+      int n = scn.nextInt(); 
+int i = 1; 
+while (i <= n){
+	System.out.print(i + " ");
+	i++; 
+}
+
+
+Q2 
+
+Write a program to print all Natural numbers from N to 1 where you have to take N as input from user
+ int n = scn.nextInt(); 
+        int i = n; 
+        while (i >= 1){
+	        System.out.print(i + " ");
+	        i--; 
+        }
+
+//  Q3 
+//  Write a program to print all odd numbers from 1 to N where you have to take N as input from user. Here N is inclusive.
+ 
+ int n = scn.nextInt(); 
+ int i = 1; 
+ while(i <= n){
+	 if (i % 2 != 0){
+		 System.out.print(i + " ");
+	 }
+	 i++; 
+ }
+ 
+
+ 
+ Q4 
+ 
+ Write a program to print all even numbers from 1 to N where you have to take N as input from the user. Note: Use while-loop OR for-loop, according to session flow.
+
+
+ 
+ int n = scn.nextInt(); 
+ int i = 1; 
+ while(i <= n){
+	 if (i % 2 == 0){
+		 System.out.print(i + " ");
+	 }
+	 i++; 
+ }
+ 
+ 
+ 
+
+ 
+ Q5 
+ 
+ Write a program to find sum all Natural numbers from 1 to N where you have to take N as input from user
+
+
+ 
+ int n = scn.nextInt(); 
+ int i = 0 ; 
+ int sum = 0 ; 
+ 
+while(i<=n){
+	sum = sum + i ; 
+	i++ ; 
+
+}System.out.println(sum);
+ 
+  */ 
+  
 
