4b122ed777b6fca69f1444115a7a0113593f7ab7
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-08 21:48:34 +0530

    Update index.java

--- While loop -1/index.java
+++ While loop -1/index.java
@@ -1,1 +1,106 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in); 
+		// while loop 
+		/*
+		
+		Q1 
+		int i = 1 ;
+		while(i <= 5){
+			System.out.println(i); 
+			i++; 
+		}
+		// System.out.println("This " + i + " is greater than 5");
+		
+		Q2
+		
+		int n = scn.nextInt(); 
+		int i = 1 ; 
+		while (i <= n){
+			System.out.println(i); 
+			i++; 
+		}
+		
+		
+		// Q3 
+		
+		int n = scn.nextInt(); 
+		int i = 0;	
+		while(i <= n){
+			if(i % 2 == 0){
+				System.out.println(i);
+			}
+			i++;	
+		}
+		
+		// Q4 
+		
+		1st approach 
+		int n = scn.nextInt(); 
+		int i = 1; 
+		while(i<=n){
+			System.out.println(i);
+			i =i + 2; 
+		}
+		
+		2nd approach 
+		
+		int n = scn.nextInt(); 
+		int i = 1 ; 
+		while(i <= n){
+			if(i %2 != 0){
+				System.out.println(i);
+			}
+			i++ ;
+		}
+		
+	
+	
+	int i = 5;
+while(i <= 10){
+    System.out.println(i);
+    i = i*2;
+}
+
+int n = scn.nextInt(); 
+int i = 1; 
+
+while(i <= n){
+	if(i % 4 == 0){
+		System.out.println(i);
+	}
+	i++; 
+}
+
+
+int n = 5 ;
+while (n >= 1){
+	System.out.println(n);
+	n-- ; 
+}
+
+int i = 1;
+while(i<=5){
+    System.out.println(i);
+    i = i-1; 
+}
+
+
+
+int i = 5; 
+while(i <= 5){
+	System.out.println(i);
+	i--;
+}
+
+
+int n = scn.nextInt(); 
+int i = n;
+while (i >= 1){
+	System.out.println(i);
+	i-- ;
+}
+
 
