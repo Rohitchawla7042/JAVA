b90a803844f1d8f2f9a2c2aa8bf49143a71000d8
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-12 16:58:58 +0530

    Update Homework..java

--- while loop-2/Homework..java
+++ while loop-2/Homework..java
@@ -48,9 +48,64 @@ You are given an integer N you need to print all the Armstrong Numbers between 1
          }
                                                           
                                                           
-  Q3
+  Q3 Write a program to input T numbers(N) from user and print first and last digits of the given numbers.
+
+
+  
+  int T = scn.nextInt();
+            int first_digit = 0 ; 
+            int last_digit = 0 ;
+            while(T > 0){
+                int n = scn.nextInt(); 
+                last_digit = n % 10 ; 
+                // System.out.print(last_digit + " "); 
+                while(n != 0){
+                    first_digit = n % 10 ; 
+                    n = n / 10 ; 
+                    
+                }T-- ;
+                System.out.println(first_digit +" " + last_digit); 
+            }
   
   
+  Q4 You have a number N, you have to write a code to find odd index digit sum and even index digit sum from given number.
+Note : Index is starting from 1 till count of digits in a number, and we consider this from right to left;
+  
+  odd = 0 ;
+         int even = 0 ; 
+         
+         int index = 1 ; 
+         while(n > 0)
+         {
+             int rem = n % 10 ; 
+             n = n / 10;        
+        
+        if(index%2 == 0 ){
+            even += rem;
+            // System.out.println("Sum of Even Index Digit :" + even);
+        }
+        else{
+            odd+= rem;
+            // System.out.println("Sum of odd Index Digit :" + odd); 
+        }
+        index++ ; 
+         }
+          System.out.println("Sum of Odd Index Digit : " + odd);
+          System.out.println("Sum of Even Index Digit : " + even);
+  
+  Q5 
+  Given a number N, each time N gets divided by 2. How many times do we need to divide N by 2 in order to get 1 as the final result ?
+
+Note : Try to solve this question using a while loop for learning purposes.
+
+  int n = scn.nextInt(); 
+           int count = 0 ; 
+           int temp = n; 
+           while(temp != 1){
+               temp = temp / 2; 
+               count++ ; 
+           }
+           System.out.println(count); 
          
 
                                                           
