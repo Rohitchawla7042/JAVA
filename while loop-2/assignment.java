307817435edc9dc656d943b83d2ab8bfc2b26373
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-26 21:17:59 +0530

    Update assignment.java

--- while loop-2/assignment.java
+++ while loop-2/assignment.java
@@ -26,29 +26,40 @@ int n = scn.nextInt();
 // After each operation print the amount left in the Bank account. If the debit amount is greater than current balance print "Insufficient Funds"(without quotes) and the operation is skipped.
 
 
-    int initial_balance = scn.nextInt(); 
-    int credit_amount ; 
-    int debit_amount = 0 ; 
-    int final_balance = 0 ; 
-    int T = scn.nextInt(); 
-    int i = 1 ;
-    while(T > 0){
+   import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in); 
+        long current_balance = scn.nextLong();
+        long debit_amount = 0 ; 
+        int type ; 
+        int T = scn.nextInt(); 
+        while (T > 0){
+            type = scn.nextInt(); 
+            if(type == 1){
+                long credit_amount = scn.nextLong(); 
+                current_balance = current_balance + credit_amount ; 
+                System.out.println(current_balance); 
+            }
+            else if(type == 2)
+            {
+                debit_amount = scn.nextLong(); 
+                if(debit_amount > current_balance){
+                    System.out.println("Insufficient Funds"); 
+                }
+                else 
+                {
+                    current_balance = current_balance - debit_amount; 
+                System.out.println(current_balance); 
+                }
+            }
+            T-- ; 
+        }
         
-   
-        if(i == 1){
-            credit_amount = scn.nextInt(); 
-            final_balance = initial_balance + credit_amount ; 
-            System.out.println(final_balance); 
-            
-        }
-        else if (i==2){
-            debit_amount = scn.nextInt(); 
-            final_balance = initial_balance + debit_amount ; 
-            System.out.println(final_balance);
-        }
-        else if(debit_amount > final_balance){
-            System.out.println("Insufficient Funds"); 
-        }
+    }
+}
         Q3 Mr. Q has a diary in which he has written a lot of numbers. He is confused about the number of digits in every number he has written. Mr. Q will provide the different numbers written in the diary and then you have to write a code to find the number of digits in every number he has written.
 
 Note: Total different Numbers are T and for every number (let's say N) you need to find the total number of digits.
