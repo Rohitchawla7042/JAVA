d7064306d3118566f31c73329df9abd7e4cc87fe
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-10-31 21:18:35 +0530

    Update Homework..java

--- Pattern -1/Homework..java
+++ Pattern -1/Homework..java
@@ -1,132 +1,105 @@
 Q1 
-Given two integers N and M as inputs, print a rectangle of N * M stars.
-
-For example if N = 3, M = 4 then pattern will be like:
-
-****
-****
-****
-
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in); 
-        int n = scn.nextInt(); 
-        int m = scn.nextInt(); 
-        for (int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                System.out.print("*");
-            }
-            System.out.println(); 
-        }
-        
-    }
-}
-
-Q2 
 
 Problem Description
 
-Given an integer N as input, print the corresponding stair pattern for N.
-
-For example if N = 4 then stair pattern will be like:
-
-*
-**
-***
-****
-
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in); 
-        int n = scn.nextInt(); 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j <= i ;j++){
-                System.out.print("*"); 
-            }
-            System.out.println(); 
-        }
-
-        
-    }
-}
-
-
-Q3 
-
-Q3. Numeric Stair Pattern
-Solved
-character backgroundcharacter
-Stuck somewhere?
-Ask for help from a TA and get it resolved.
-Get help from TA.
-Problem Description
-
-Given an integer N as input, print the corresponding pattern for N.
+Given an integer N, print the corresponding Inverted Half Pyramid pattern for N.
 
 For example if N = 4 then pattern will be like:
 
-1
-1 2
-1 2 3
-1 2 3 4
-  
-  import java.lang.*;
+****
+***
+**
+*
+
+
+import java.lang.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in); 
         int n = scn.nextInt(); 
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= i ; j++){
-                if(i == j){
-                    System.out.print(j); 
-                }
-                else
-                System.out.print(j+" ");
-            }
-            System.out.println(""); 
-        }
+for(int i = n ; i > 0 ; i--){
+	for (int j = 0 ; j <i ; j++ ){
+		System.out.print("*");
+	}
+	System.out.println(); 
+}
+// System.out.println(); 
+        
     }
 }
 
-Q4 
+Q2
+
 
 Problem Description
 Given an integer N as input, print the corresponding pattern for N.
 
 For example if N = 5 then pattern will be like:
 
-1
-1_
-1_3
-1_3_
-1_3_5
-Note : Here '_' represents space for explanation purpose only. You have to print space in your code.
-  
-  import java.lang.*;
+____*
+___**
+__***
+_****
+*****
+
+import java.lang.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in); 
-        int n = scn.nextInt();
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= i ; j++){
-				if(j %2 != 0)
-                System.out.print(j+"");
-				else
-				System.out.print(" ");
+        int N = scn.nextInt(); 
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                if (j < N-i+1)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("*");
+                }
             }
-
-            System.out.println(); 
+            System.out.println("");
         }
+       
+    }
+}
+
+Q3
+
+Problem Description
+Given an integer N as input, print the corresponding pattern for N.
+
+For example if N = 5 then pattern will be like:
+
+*****
+_****
+__***
+___**
+____*
+Note : Here '_' represents space for explanation purpose only. You have to print space in your code.
+	
+	import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in); 
+        int n  = scn.nextInt(); 
+		for(int i = 0 ; i < n ; i++){
+			for (int j = 1 ; j <= i ; j++){
+				System.out.print(" ");
+			}
+			for(int j = i ; j < n ;j++){
+				System.out.print("*"); 
+			}
+			System.out.println(); 
+		}
     }
 }
 
