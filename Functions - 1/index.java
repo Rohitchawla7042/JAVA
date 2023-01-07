abff69e43e130a4f4191b76ae884df5cf8c836fd
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2023-01-07 18:12:34 +0530

    Update index.java

--- Functions - 1/index.java
+++ Functions - 1/index.java
@@ -145,4 +145,77 @@ for ex math.ceil(5.6) = 6
 }
 
 	}
+	
+
+// new functions 
+import java.util.* ; 
+// Scanner scn = new Scanner(System.in); 
+// Q1 count the nu of digits using functions 
+/*
+import java.util.* ; 
+class Main 
+{
+	static int count_type(int a)
+	{
+		int count = 0 ; 
+		while (a > 0 )
+		{
+			count++;	
+			a = a / 10 ; 
+		}
+		return count ; 
+	}
+	public static void main (String args[])
+	{
+		Scanner scn = new Scanner(System.in); 
+		int n = scn.nextInt(); 
+		int ans = count_type(n); 
+		System.out.println(ans); 
+	}
+}
+
+// Q2 Write a function that takes 2 input and return the sum of 2 numbers 
+
+class Main {
+	
+	static int sum_digits(int a , int b){
+		int add = 0 ; 
+		add = a + b ; 
+		return add ; 
+	}	
+	// return add ; 
+	
+	public static void main (String args[]){
+		Scanner scn = new Scanner(System.in); 
+		int x = scn.nextInt(); 
+		int y = scn.nextInt(); 
+		int sum = sum_digits(x , y); 
+		System.out.println(sum); 
+		
+	}
+}
+
+//  pre defined functions 
+
+// Math.pow(a,b); a^ b 
+// Math.pow(2,4); 2^4 = 2*2*2*2 ; 
+
+int ans = (int)Math.pow(2 , 4); 
+System.out.println(ans); 
+
+ceil vaule - >  first integer greater than or equal to given number . 
+
+3.14 - > 4 
+4.98 - > 5 
+2.001 - >  3 
+9.0 - > 9 
+
+floor value - > first integer smaller than or equal to given number 
+
+3.14 - > 3 
+5.67 - > 5 
+*/
+
+
+
 
