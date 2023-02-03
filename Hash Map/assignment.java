7418ae6474571097c1378544f49154d037e0a3f6
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2023-02-03 22:53:58 +0530

    Create assignment.java

---
+++ Hash Map/assignment.java
@@ -0,0 +1,29 @@
+Q1 
+
+You are given an array A of N integers. Return the count of elements with frequncy 1 in the given array.
+  
+ public class Solution {
+    public int solve(int[] A) {
+      HashMap<Integer , Integer> map = new HashMap<>(); 
+      for(int i = 0 ; i < A.length ; i++) 
+      {
+        if(map.containsKey(A[i]==true)) 
+        {
+          int old_freq = 1 ; 
+          map.put(A[i] , old_freq+1) ; 
+        }
+        else 
+        {
+          map.put(A[i] , 1) ; 
+        }
+      }
+      int count = 0 ; 
+      for(int key : map.KeySet()) 
+      {
+        if(map.get(key)==1)
+        {
+          count++; 
+        }
+      }
+      return count ; 
+        
